2a44d44693362e63a4e236ed90f9585c15f1503f
Akiyoshi Tanaka <akkunchoi@gmail.com>
2014-03-12 01:59:10 +0900

    add sample code

---
+++ sample.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : '',
+  password : '',
+  database : ''
+});
+
+connection.connect();
+
+var Table = require('./lib/table');
+
+var User = new Table({tableName: 'users', adapter: connection});
+User.find({}, function(err, users){
+  console.log(users);
+});
+
+connection.end();
